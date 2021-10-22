890ef5cc481d60b7b706f1bcb45633829fd363fb
Yérim Konte <konteyerim@gmail.com>
2021-10-22 11:12:18 +0000

    vinn

--- WidraSoftCloud.UI/Models/PesageControle.cs
+++ WidraSoftCloud.UI/Models/PesageControle.cs
@@ -13,13 +13,13 @@ namespace WidraSoftCloud.UI.Models
         public string UniqueKey { get; set; }
         public string CategorieCam { get; set; }
         public int PoidsTotalMaxAut { get; set; }
-        public int Rang1 { get; set; }
+        public string Rang1 { get; set; }
         public int Poids1 { get; set; }
-        public int Rang2 { get; set;}
+        public string Rang2 { get; set;}
         public int Poids2 { get; set; }
-        public int Rang3 { get; set; }
+        public string Rang3 { get; set; }
         public int Poids3 { get; set; }
-        public int Rang4 { get; set; }
+        public string Rang4 { get; set; }
         public int Poids4 { get; set; }
         [Display(Name = "Date synchronisation")]
         public DateTime DateSynchronisation { get; set; }
